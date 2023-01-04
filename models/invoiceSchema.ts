14dbe2e1b7e56d2901f2ab29ba717a8891ea9ced
nikhea <imonikheaugbodaga@gmail.com>
2023-01-04 01:17:27 +0100

    created mongosse invoice shcema

---
+++ models/invoiceSchema.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const InvoiceSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    invoiceId: { type: String, required: true },
+    createdAt: { type: String, required: true },
+    paymentDue: { type: String },
+    description: { type: String },
+    paymentTerms: { type: Number },
+    clientName: { type: String },
+    clientEmail: { type: String },
+    status: { type: String, required: true },
+    senderAddress: {
+      street: String,
+      city: String,
+      postCode: String,
+      country: String,
+    },
+    clientAddress: {
+      street: String,
+      city: String,
+      postCode: String,
+      country: String,
+    },
+    items: [{ name: String, quantity: Number, price: Number, total: Number }],
+    total: Number,
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Invoice ||
+  mongoose.model("Invoice", InvoiceSchema);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,121 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import clientPromise from "../lib/database/dbConnect";
-import { InferGetServerSidePropsType } from "next";
-
-export async function getServerSideProps() {
-  try {
-    await clientPromise;
-  
-    
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-}
-const Home = ({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+import dbConnect from "../lib/database/dbConnect";
+import Invoice from "../models/invoiceSchema";
+const Home = ({ invoice }: any) => {
+  console.log(invoice, "invoice");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-            for instructions.
-          </h2>
-        )}
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      <main></main>
     </div>
   );
 };
 
 export default Home;
+export async function getServerSideProps() {
+  await dbConnect();
+  /* find all the data in our database */
+  const result = await Invoice.find({});
+  const invoice = result.map((doc) => {
+    const invoice = doc.toObject();
+    invoice._id = invoice._id.toString();
+    return invoice;
+  });
+
+  return { props: { invoice } };
+}
