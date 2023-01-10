3c801d4ec6464305ab79bab84a9dc8f4d13f2559
nikhea <imonikheaugbodaga@gmail.com>
2023-01-10 01:42:33 +0100

    comment our server

--- lib/database/dbConnect.ts
+++ lib/database/dbConnect.ts
@@ -1,20 +1,23 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import * as mongoose from "mongoose";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+// import * as mongoose from "mongoose";
 
-let dev_db_url = process.env.MONGODB_URI_LOCAL;
-// const mongoDB = dev_db_url || process.env.MONGODB_URI;
-// const mongoDB = process.env.MONGODB_URI;
-const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-mongoose
-  .connect(dev_db_url , connectionParams)
-  .then(() => {
-    console.log("Connected to database ");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. \n${err}`);
-  });
-export const db = mongoose.connection;
+// // let dev_db_url = process.env.MONGODB_URI_LOCAL;
+// // const mongoDB = dev_db_url || process.env.MONGODB_URI;
+// // const mongoDB = process.env.MONGODB_URI;
+// let MONGODB_URI_LOCAL = "mongodb://localhost:27017/InvoicesDatabase";
+
+// const connectionParams: any = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+// mongoose
+//   .connect(MONGODB_URI_LOCAL, connectionParams)
+//   .then(() => {
+//     console.log("Connected to database ");
+//   })
+//   .catch((err) => {
+//     console.error(`Error connecting to the database. \n${err}`);
+//   });
+// export const db = mongoose.connection;
+export let sddsd:string = "dsf";
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,47 +1,49 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { MongoClient } from "mongodb";
-import dbConnect from "../../../lib/database/dbConnect";
-import { compare } from "bcryptjs";
+// import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
+// import { MongoClient } from "mongodb";
+// import dbConnect from "../../../lib/database/dbConnect";
+// import { compare } from "bcryptjs";
 
-export default NextAuth({
-  //Configure JWT
-  session: {
-    // { // @ts-ignore}
-    jwt: true,
-  },
-  //Specify Provider
-  providers: [
-    // { // @ts-ignore}
-    Providers.Credentials({
-      async authorize(credentials: { email: any; passowrd: string }) {
-        //Connect to DB
-        const db = await await dbConnect();
-        //Get all the users
-        const users = await db.collection("users");
-        //Find user with the email
-        const result = await users.findOne({
-          email: credentials.email,
-        });
-        //Not found - send error res
-        if (!result) {
-          db.close();
-          throw new Error("No user found with the email");
-        }
-        //Check hased password with DB password
-        const checkPassword = await compare(
-          credentials.passowrd,
-          result.passowrd
-        );
-        //Incorrect password - send response
-        if (!checkPassword) {
-          db.close();
-          throw new Error("Password doesnt match");
-        }
-        //Else send success response
-        db.close();
-        return { email: result.email };
-      },
-    }),
-  ],
-});
+// export default NextAuth({
+//   //Configure JWT
+//   session: {
+//     // { // @ts-ignore}
+//     jwt: true,
+//   },
+//   //Specify Provider
+//   providers: [
+//     // { // @ts-ignore}
+//     Providers.Credentials({
+//       async authorize(credentials: { email: any; passowrd: string }) {
+//         //Connect to DB
+//         const db = await await dbConnect();
+//         //Get all the users
+//         const users = await db.collection("users");
+//         //Find user with the email
+//         const result = await users.findOne({
+//           email: credentials.email,
+//         });
+//         //Not found - send error res
+//         if (!result) {
+//           db.close();
+//           throw new Error("No user found with the email");
+//         }
+//         //Check hased password with DB password
+//         const checkPassword = await compare(
+//           credentials.passowrd,
+//           result.passowrd
+//         );
+//         //Incorrect password - send response
+//         if (!checkPassword) {
+//           db.close();
+//           throw new Error("Password doesnt match");
+//         }
+//         //Else send success response
+//         db.close();
+//         return { email: result.email };
+//       },
+//     }),
+//   ],
+// });
+
+export let sddsd:string = "dsf";
